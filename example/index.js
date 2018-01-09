624b6082fc4a99a1c77d53fa4753d5271adb3e63
Carlos <cnavidad93@gmail.com>
2018-01-10 00:17:39 +0100

    update example

---
+++ example/index.js
@@ -0,0 +1,64 @@
+const socket = io(`http://${window.location.hostname}:3010`);
+let joined = false;
+let totalUser = 0;
+
+socket.emit('log_in', {
+  name: 'eudago'
+});
+
+socket.on('room_joined', (room) => {
+  let $urlRoom = document.getElementById('url-room');
+  let $usersRoom = document.getElementById('users-room');
+
+  joined = true;
+  totalUser++;
+  $urlRoom.innerHTML = `@${room.name}`;
+  $usersRoom.innerHTML = totalUser;
+
+  changeView();
+  console.log('joined to room: ', room.name);
+});
+
+socket.on('notification', (message) => {
+  console.log(message);
+});
+
+function createRoom() {
+  if(!joined) {
+    socket.emit('create_room');
+  }
+}
+function leaveRoom() {
+  if(joined) {
+    joined = false;
+    totalUser--;
+    socket.emit('leave_room');
+
+    changeView();
+  }
+}
+function joinRoom() {
+  let $input = document.getElementById('room-name').value;
+  if(!joined) {
+    joined = true;
+    totalUser++;
+    socket.emit('join_room', $input);
+  }
+}
+
+
+function validateInput(elm){
+  let $input = document.getElementById('room-name').value;
+  if($input.length > 3){
+    document.getElementById('join-button').disabled = false;
+  }else{
+    document.getElementById('join-button').disabled = true;
+  }
+}
+function changeView(){
+  document.getElementById('room-page').classList.toggle('fade-in');
+  document.getElementById('room-page').classList.toggle('fade-out');
+
+  document.getElementById('login-page').classList.toggle('fade-in');
+  document.getElementById('login-page').classList.toggle('fade-out');
+}
